940cb5ca5f7c46766bd9b711159b59799d3169ff
Daniel San Martin <dsanmartinreyes@gmail.com>
2017-07-12 19:13:17 -0400

    Graficos de comparacion

--- python/plots.py
+++ python/plots.py
@@ -5,8 +5,9 @@ from mpl_toolkits.mplot3d import Axes3D
 from matplotlib.collections import LineCollection
 from matplotlib import cm
 
-def plot(df1, df2, df3, df4):
+def plotTN(df1, df2, df3, df4):
     fig, ax = plt.subplots()
+    plt.title("Tiempo vs N")
     plt.plot(df1["n"], df1["time_real"], "k-x", label="1 thread")
     plt.plot(df2["n"], df2["time_real"], "b-*", label="2 threads")
     plt.plot(df3["n"], df3["time_real"], "r-d", label="3 threads")
@@ -19,16 +20,57 @@ def plot(df1, df2, df3, df4):
     plt.show()
     return None
 
+def plotTT(df1, df2, df3, df4):
+    fig, ax = plt.subplots()
+    plt.title("Tiempo vs Threads")
+    n_threads = np.arange(1, 5)
+    data = np.zeros((4, 4))
+    
+    for i in range(4):
+        data[i] = np.array([df1["time_real"][i], df2["time_real"][i], 
+                           df3["time_real"][i], df4["time_real"][i]])
+
+    plt.plot(n_threads, data[0], "k-x", label="N=100")
+    plt.plot(n_threads, data[1], "b-*", label="N=500")
+    plt.plot(n_threads, data[2], "r-d", label="N=1000")
+    plt.plot(n_threads, data[3], "g-o", label="N=2000")
+    
+    ax.set_xticks(n_threads)
+    plt.grid(True)
+    plt.legend()
+    plt.xlabel("Threads")
+    plt.ylabel("t [s]")
+    plt.show()
+    return None
+
+def plotSpeedup(df1, df2, df3, df4):
+    fig, ax = plt.subplots()
+    plt.title("Speedup vs Threads")
+    n_threads = np.arange(1, 5)
+
+    plt.plot(n_threads, df1["time_real"]/df2["time_real"], "b-*", label="SUP 2 threads")
+    plt.plot(n_threads, df1["time_real"]/df3["time_real"], "r-d", label="SUP 3 threads")
+    plt.plot(n_threads, df1["time_real"]/df4["time_real"], "g-o", label="SUP 4 threads")
+    
+    ax.set_xticks(n_threads)
+    plt.grid(True)
+    plt.legend()
+    plt.xlabel("Threads")
+    plt.ylabel("Speedup")
+    plt.show()
+    return None
+    
+
 def plot3D(x, t, u):
     fig = plt.figure(figsize=(12,6))
     ax = fig.gca(projection='3d')
-    #X, T = np.meshgrid(xx, t)
     X, T = np.meshgrid(x, t)
     ax.plot_surface(X, T, u, rstride=1, cstride=1, color="white")
     ax.set_xlabel('x')
     ax.set_ylabel('t')
     ax.view_init(elev=30., azim=230.)
     plt.show()
+    return None
 
 csv_file1 = "../results/n_1/20170709.csv"
 csv_file2 = "../results/n_2/20170709.csv"
@@ -38,7 +80,9 @@ df1 = pd.read_csv(csv_file1)
 df2 = pd.read_csv(csv_file2)
 df3 = pd.read_csv(csv_file3)
 df4 = pd.read_csv(csv_file4)
-plot(df1, df2, df3, df4)
+plotTN(df1, df2, df3, df4)
+plotTT(df1, df2, df3, df4)
+plotSpeedup(df1, df2, df3, df4)
 
 #csv_x = "../C/CSV/x.csv"
 #csv_t = "../C/CSV/t.csv"
